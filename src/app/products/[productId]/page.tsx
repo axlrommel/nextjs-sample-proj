9e0aa9937fa3cfea2f2a8e7235e0d5f337e21dcb
axlrommel <rommelvillagomez77@gmail.com>
2024-08-05 20:34:56 -0500

    initial checkin

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,35 @@
+import data from '@/mock/products.json';
+
+const productDetail = ({ params }: { params: { productId: string } }) => {
+  const product = data.find((item: any) => item.id === params.productId);
+  if (!product) {
+    return <p>Product not Found</p>;
+  }
+
+  return (
+    <div className='flex min-h-screen flex-col p-24'>
+      <h1 className='text-2xl font-semibold'>Product Description</h1>
+      <h3 className={`mb-3 text-xl `}>{product.name}</h3>
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+        Price: {product.price}
+      </p>
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+        Description: {product.description}
+      </p>
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+        Category: {product.category}
+      </p>
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+        Rating: {product.rating}
+      </p>
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+        Reviews: {product.numReviews}
+      </p>
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+        Stock: {product.countInStock}
+      </p>
+    </div>
+  );
+};
+
+export default productDetail;
